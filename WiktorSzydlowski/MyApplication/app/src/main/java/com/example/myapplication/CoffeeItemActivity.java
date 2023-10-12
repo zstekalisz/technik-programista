48fbb57bdc217b63c86be32d5f5c6ec44af6472f
Wiktor Szydlowski <wiktorszydlowski@mail.com>
2023-10-12 19:42:28 +0200

    projekt

---
+++ WiktorSzydlowski/MyApplication/app/src/main/java/com/example/myapplication/CoffeeItemActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.model.Coffee;
+import com.example.utils.StaticDataBase;
+
+public class CoffeeItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffee_item);
+        Intent intent = getIntent();
+        int index = intent.getIntExtra("index", 0);
+        int type = intent.getIntExtra("type", 0);
+        Coffee coffee;
+
+
+
+        switch (type){
+            default:
+            case 0:
+                coffee = StaticDataBase.coffies.get(index);
+                break;
+            case 1:
+                coffee = StaticDataBase.driny.get(index);
+                break;
+            case 2:
+                coffee = StaticDataBase.zagrycha.get(index);
+                break;
+        }
+        ImageView imageView = (ImageView) findViewById(R.id.coffee);
+        TextView desc = (TextView) findViewById(R.id.description);
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(coffee.getName());
+        desc.setText(coffee.getDescription());
+        imageView.setImageResource(coffee.getPicture());
+        
+
+    }
+}
