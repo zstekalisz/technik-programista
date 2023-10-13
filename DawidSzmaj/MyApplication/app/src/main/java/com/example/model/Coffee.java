bfa62fa870187b48d9617860661ada6dcd539896
Dawid Szmaj <dsdasda@op.pl>
2023-10-13 16:32:35 +0200

    projekt

---
+++ DawidSzmaj/MyApplication/app/src/main/java/com/example/model/Coffee.java
@@ -0,0 +1,52 @@
+package com.example.model;
+
+public class Coffee {
+    private int id;
+    private String name;
+    private String description;
+    private int picture;
+
+    public Coffee(int id, String name, String description, int picture) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.picture = picture;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+
+    public void setPicture(int picture) {
+        this.picture = picture;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
