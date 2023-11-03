12d3893eebe6051913ca2d51662462e176b8b0e2
Radosław Kuświk <rkuswik@zste.kalisz.pl>
2023-11-03 08:25:00 +0100

    Przyrost

--- AplikacjeKonsolowe/src/pl/zste/zadanieegzamin/arkusz4/WyszukiwanieWTablicy.java
+++ AplikacjeKonsolowe/src/pl/zste/zadanieegzamin/arkusz4/WyszukiwanieWTablicy.java
@@ -1,8 +1,28 @@
 package pl.zste.zadanieegzamin.arkusz4;
 
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class WyszukiwanieWTablicy {
 
 	public static void main(String[] args) {
+		System.out.println("Program wyszukujący wprowadzoną liczbę w tablicy losowych liczb");
+		int[] tab = inicjacja();
+		//testujemyInicjalizacje(tab);
+		Scanner sc = new Scanner(System.in);
+		boolean wartoscSzukanaOk= false;
+		while(!wartoscSzukanaOk) {
+			System.out.print("Wpisz liczbę którą będę szukać :");
+			try {
+				int szukana = sc.nextInt();
+				wartoscSzukanaOk = true;
+				
+			}catch(InputMismatchException e) {
+				System.out.println("Wprowadź poprawną liczbę");
+			}
+			
+		}
 		
 
 	}
@@ -16,7 +36,19 @@ public class WyszukiwanieWTablicy {
 	}
 	
 	public static int[] inicjacja() {
-		return null;
+		int[] tablica = new int[51];
+		Random r = new Random();
+		for(int i =0; i < tablica.length-1; i++) {
+			int nextInt = r.nextInt(100);
+			tablica[i] = nextInt;
+		}
+		return tablica;
+	}
+	
+	public static void testujemyInicjalizacje(int[] tab) {
+		for(int i = 0; i< tab.length; i++) {
+			System.out.println(tab[i]);
+		}
 	}
 
 }
