0a2972122bc953f7eb834346ba4ccb0256399109
Radosław Kuświk <rkuswik@zste.kalisz.pl>
2023-11-03 07:55:50 +0100

    Arkusz wiosna 2022 -01  i zima 2022

--- AplikacjeKonsolowe/src/pl/zste/zadanieegzamin/arkusz3/SortowanieMalejace.java
+++ AplikacjeKonsolowe/src/pl/zste/zadanieegzamin/arkusz3/SortowanieMalejace.java
@@ -25,7 +25,13 @@ public class SortowanieMalejace {
 		}
 		sc.close();
 	}
-	
+	/************************************************
+	 * nazwa funkcji : sortMyTable - sortuje tablice w obiekcie {@link SortowanieMalejace#tablica}
+	 * @param - brak
+	 * @return - void
+	 * @author Radek 1234556798 
+	 * 
+	 ************************************************/
 	public void sortMyTable() {
 		for(int i = 0; i<tablica.length; i++) {
 			int indexOfMax = getIndexOfMax(tablica, i);
@@ -34,6 +40,14 @@ public class SortowanieMalejace {
 			tablica[i]= valueBuff;
 		}
 	}
+	/*************************************************
+	 * @apiNote - nazwa funkcji getIndexOfMax - funkcja szuka w tablicy wartości maksymalnej w przedziale od tabStartIndex<br>
+	 * do końca tablicy i zwraca indeks wartości max.
+	 * @param tab - tablica w której szukamy wartości maksymalnej
+	 * @param tabStartIndex - indeks od którego zaczynamy szukać wartości maksymalnej
+	 * @return indeks wartości maksymalnej w zadanym przedziale
+	 * @author Radek 1234556798 
+	 *************************************************/
 	private int getIndexOfMax(int[] tab, int tabStartIndex) {
 		int result = tab[tabStartIndex];
 		int resultIndex = tabStartIndex;
---
+++ AplikacjeKonsolowe/src/pl/zste/zadanieegzamin/arkusz4/WyszukiwanieWTablicy.java
@@ -0,0 +1,22 @@
+package pl.zste.zadanieegzamin.arkusz4;
+
+public class WyszukiwanieWTablicy {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static int szukajElementu(int[] tablica) {
+		return 0;
+	}
+	
+	public static void dodajWartownika(int wartownik, int[]tablica) {
+		
+	}
+	
+	public static int[] inicjacja() {
+		return null;
+	}
+
+}
