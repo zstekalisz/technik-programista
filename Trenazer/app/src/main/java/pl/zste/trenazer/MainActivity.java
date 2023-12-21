d24c7cb64a00d38253a753fdd02e9ab6779e931a
Radosław Kuświk <rkuswik@zste.kalisz.pl>
2023-12-21 07:49:23 +0100

    trenazer - 21.12.2023

--- Trenazer/app/src/main/java/pl/zste/trenazer/MainActivity.java
+++ Trenazer/app/src/main/java/pl/zste/trenazer/MainActivity.java
@@ -2,13 +2,26 @@ package pl.zste.trenazer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import pl.zste.model.TrenagerListClicker;
+
+public class MainActivity extends AppCompatActivity implements TrenagerListClicker {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setTraining(int indexOfWorkout) {
+        //Intent intent = new Intent(this, null);
+        //intent.putExtra("index", indexOfWorkout);
+        //startActivity(intent);
+        Log.i("TEST", "Kliknięto "+indexOfWorkout+ " element listy");
     }
 }
