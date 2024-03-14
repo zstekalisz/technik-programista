aefbe10649b4660e42bb799d3a96e375389e7006
Radosław Kuświk <rkuswik@zste.kalisz.pl>
2024-03-14 09:03:41 +0100

    aplikacje - gesty #3

--- WloskieConieco/app/src/main/java/pl/zste/fragments/StoresFragment.java
+++ WloskieConieco/app/src/main/java/pl/zste/fragments/StoresFragment.java
@@ -8,18 +8,19 @@ import androidx.fragment.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import pl.zste.app.R;
 
 
 public class StoresFragment extends ListFragment {
 
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(),
+                R.layout.single_pasta_item, getResources().getStringArray(R.array.cities));
+        setListAdapter(adapter);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
