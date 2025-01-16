260c52343457adba20dcc2cda975d831fcb6bf13
Radosław Kuświk <rkuswik@zste.kalisz.pl>
2025-01-16 12:20:35 +0100

    egzamin inf04

--- AplikacjeDesktopowe4D/src/pl/zste/desktop/cwiczenia/TestOdczytuZPliku.java
+++ AplikacjeDesktopowe4D/src/pl/zste/desktop/cwiczenia/TestOdczytuZPliku.java
@@ -7,6 +7,7 @@ public class TestOdczytuZPliku {
 
 	public static void main(String[] args) {
 		TestOdczytuZPliku zPliku = new TestOdczytuZPliku();
+		InputStream resourceAsStream = TestOdczytuZPliku.class.getClassLoader().getResourceAsStream("counter.txt");
 		InputStream inputStream = zPliku.getClass().getClassLoader().getResourceAsStream("counter.txt");
 		//System.out.println(inputStream);
 		Scanner sc = new Scanner(inputStream);
