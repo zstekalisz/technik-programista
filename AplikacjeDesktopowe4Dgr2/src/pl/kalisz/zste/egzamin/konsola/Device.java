b68d03b38336e1693597af33c42310462d04d6ab
Radosław Kuświk <rkuswik@zste.kalisz.pl>
2025-02-23 16:51:05 +0100

    Aplikacja na zajęcia 24.02.2025

--- AplikacjeDesktopowe4Dgr2/src/pl/kalisz/zste/egzamin/konsola/Device.java
+++ AplikacjeDesktopowe4Dgr2/src/pl/kalisz/zste/egzamin/konsola/Device.java
@@ -2,6 +2,13 @@ package pl.kalisz.zste.egzamin.konsola;
 
 public class Device {
 	
+	/**
+	 * nazwa: showMessage
+	 * opis: metoda przyjmuje parametr i wyśietla go w konsoli
+	 * parametry:  @param message - typ String - wiadomość do wyświetlenia w konsoli
+	 * zwracany typ : void
+	 * autor: @author Radek
+	 */
 	public void showMessage(String message) {
 		System.out.println(message);
 	}
--- AplikacjeDesktopowe4Dgr2/src/pl/kalisz/zste/egzamin/konsola/WashingMachine.java
+++ AplikacjeDesktopowe4Dgr2/src/pl/kalisz/zste/egzamin/konsola/WashingMachine.java
@@ -5,6 +5,7 @@ public class WashingMachine extends Device {
 	private int programOption = 0;
 
 	public int setProgramOption(int programOption) {
+		//sprawdzam czy parametr miesci się w zakresie od 1 do 12
 		if(programOption>=1 && programOption<=12) {
 			this.programOption = programOption;
 		}else {
