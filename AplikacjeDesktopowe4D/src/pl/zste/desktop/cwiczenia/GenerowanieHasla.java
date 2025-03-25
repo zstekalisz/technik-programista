a3f0762c8b96cbfefff463d8be5aaf5dca172ae5
Radosław Kuświk <rkuswik@zste.kalisz.pl>
2025-03-25 08:42:21 +0100

    poprawka w generowaniu hasla

--- AplikacjeDesktopowe4D/src/pl/zste/desktop/cwiczenia/GenerowanieHasla.java
+++ AplikacjeDesktopowe4D/src/pl/zste/desktop/cwiczenia/GenerowanieHasla.java
@@ -5,28 +5,39 @@ import java.util.Random;
 public class GenerowanieHasla {
 
 	public static void main(String[] args) {
-		String haslo = generujHaslo(8, true, true, true);
+		String haslo = generujHaslo(2, true, true, true);
 		System.out.println(haslo);
 
 	}
 	public static String generujHaslo(int ileZnakow, boolean litery, boolean cyfry, boolean specjalne) {
-		String literki = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+		String literki = "qwertyuiopasdfghjklzxcvbnm";
+		String duze = "QWERTYUIOPASDFGHJKLZXCVBNM";
 		String cyferki = "1234567890";
 		String specjalki = "!@#$%^&*()_=+";
-		String wynik = "";
-		if(litery) {
-			wynik = wynik+literki;				
-		}
-		if(cyfry) {
-			wynik = wynik+cyferki;
-		}
-		if(specjalne) {
-			wynik = wynik+specjalki;
-		}
-		String[] znaki = wynik.split("");
+		
+		String[] znaki = literki.split("");
 		
 		String haslo = "";
 		Random r = new Random();
+		if(litery && ileZnakow>0) {
+			int index = r.nextInt(duze.length());
+			String znak = duze.split("")[index];
+			haslo = haslo + znak;
+			ileZnakow--;		
+		}
+		if(cyfry && ileZnakow>0) {
+			int index = r.nextInt(cyferki.length());
+			String znak = cyferki.split("")[index];
+			haslo = haslo + znak;
+			ileZnakow--;
+		}
+		if(specjalne && ileZnakow>0) {
+			int index = r.nextInt(specjalki.length());
+			String znak = specjalki.split("")[index];
+			haslo = haslo + znak;
+			ileZnakow--;
+		}
+		
 		for(int i=0; i<ileZnakow; i++) {
 			int index = r.nextInt(znaki.length);
 			String znak = znaki[index];
